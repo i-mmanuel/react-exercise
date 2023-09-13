63de0fd2fec90bd64b49cf8217d310ef571f3f51
Dan Lawson <daniel.lawson@cancer.org.uk>
2023-09-13 11:03:09 +0100

    Updated packages and removed JEST for purposes of it being replaced soon

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,26 +1,26 @@
-import { Heading, Text, Box } from "@cruk/cruk-react-components";
-import { NasaSearchParams } from "../../types";
-import Results from "../Results";
+import { Heading, Text, Box } from "@cruk/cruk-react-components"
+import { NasaSearchParams } from "../../types"
+import Results from "../Results"
 
 export const HomePage = () => {
-  //
-  // form and validation code here
+	//
+	// form and validation code here
 
-  const exampleParam: NasaSearchParams = {
-    keywords: "moon",
-    yearStart: 2000,
-    mediaType: "image",
-  };
+	const exampleParam: NasaSearchParams = {
+		keywords: "moon",
+		yearStart: 2000,
+		mediaType: "image",
+	}
 
-  return (
-    <Box marginTop="s" paddingTop="s">
-      <Heading h1>React Exercise</Heading>
+	return (
+		<Box marginTop="s" paddingTop="s">
+			<Heading h1>React Exercise</Heading>
 
-      <Text>Form goes here</Text>
+			<Text>Form goes here</Text>
 
-      <Results searchParams={exampleParam} />
-    </Box>
-  );
-};
+			<Results searchParams={exampleParam} />
+		</Box>
+	)
+}
 
-export default HomePage;
+export default HomePage
