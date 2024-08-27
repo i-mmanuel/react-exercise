e2bf65c65bc9d2e35471d954e4e145768d304869
develliot <elliot.revan@cancer.org.uk>
2024-08-27 14:06:43 +0100

    simplify stylesheet manager context

--- src/contexts/StyleSheetManager.tsx
+++ src/contexts/StyleSheetManager.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import React, { useState } from "react";
-import { useServerInsertedHTML } from "next/navigation";
 import {
-  IStyleSheetManager,
-  ServerStyleSheet,
-  ShouldForwardProp,
+  type IStyleSheetManager,
+  type ShouldForwardProp,
   StyleSheetManager as SSM,
 } from "styled-components";
-import isPropValid from "@emotion/is-prop-valid/dist/declarations/src";
-
-type StyledComponentsRegistryProps = {
-  children: React.ReactNode;
-};
+import isPropValid from "@emotion/is-prop-valid";
 
 const shouldForwardPropFunc: ShouldForwardProp<"web"> = (propName, target) => {
   if (typeof target === "string") {
@@ -37,26 +28,4 @@ const StyleSheetManager = ({
   </SSM>
 );
 
-export const StyledComponentsRegistry = ({
-  children,
-}: StyledComponentsRegistryProps) => {
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
-
-  useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement();
-    styledComponentsStyleSheet.instance.clearTag();
-    return <>{styles}</>;
-  });
-
-  if (typeof window !== "undefined") return <>{children}</>;
-
-  return (
-    <StyleSheetManager
-      shouldForwardProp={shouldForwardPropFunc}
-      enableVendorPrefixes={enableVendorPrefixes}
-      sheet={sheet}
-    >
-      {children}
-    </StyleSheetManager>
-  );
-};
+export default StyleSheetManager;
