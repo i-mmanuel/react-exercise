d2c39dd37a56726a65f0d7c598770b16200279d7
i-mmanuel <emmanuel.jnr@outlook.com>
2024-12-03 15:03:19 +0000

    Better formatting for Nasa Data

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,29 +1,46 @@
 "use client";
 
-import { Text } from "@cruk/cruk-react-components";
+import { Collapse, Loader, ErrorText } from "@cruk/cruk-react-components";
 import { NasaResponse, NasaSearchParams } from "../types";
 import { urlNasaSearch } from "../services/nasa";
 import { useQuery } from "@tanstack/react-query";
 
 export function List() {
-  const values: NasaSearchParams = {
-    keywords: "moon",
-    mediaType: "audio",
-    yearStart: 2000,
-  };
+	const values: NasaSearchParams = {
+		keywords: "moon",
+		mediaType: "audio",
+		yearStart: 2000,
+	};
 
-  const urlNasaSearchUrl = values
-    ? urlNasaSearch(values as NasaSearchParams)
-    : "";
+	const urlNasaSearchUrl = values ? urlNasaSearch(values as NasaSearchParams) : "";
 
-  console.log(urlNasaSearchUrl);
+	console.log(urlNasaSearchUrl);
 
-  const { data } = useQuery<NasaResponse>(
-    ["nasaSearch", values],
-    () => fetch(urlNasaSearchUrl).then((res) => res.json()),
-    { enabled: !!urlNasaSearchUrl.length },
-  );
+	const { data, isLoading, error } = useQuery<NasaResponse>(
+		["nasaSearch", values],
+		() => fetch(urlNasaSearchUrl).then(res => res.json()),
+		{ enabled: !!urlNasaSearchUrl.length }
+	);
 
-  // TODO somehow render results
-  return <>{!!data && <Text>{JSON.stringify(data)}</Text>}</>;
+	if (isLoading) {
+		return <Loader />;
+	}
+
+	if (error) {
+		return <ErrorText>Error loading data</ErrorText>;
+	}
+
+	// TODO somehow render results
+	const showNasaData = () =>
+		!!data &&
+		data.collection.items.map(
+			item =>
+				item.data[0] && (
+					<Collapse id={item.data[0].nasa_id} key={item.data[0].nasa_id} headerTitleText={item.data[0].title}>
+						{item.data[0].description}
+					</Collapse>
+				)
+		);
+
+	return <>{!!data && showNasaData()}</>;
 }
